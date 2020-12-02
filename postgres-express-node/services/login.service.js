acbb9b217aa98a4c4384ea70c242d1fa0448a557
IBudisa <ibudis03@fesb.hr>
2020-12-02 12:56:01 +0100

    Add JWT token verification using express-jwt

--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -34,7 +34,7 @@ class LoginService {
       const token = this.generateToken(payload);
       return {user,token};
     }
-
+    this.logger.error("Invalid password");
     throw new Error("Authentication failed");
   }
 
