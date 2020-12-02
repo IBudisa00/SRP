ca957e9506360c48386d1daa95a2876e71084314
IBudisa <ibudis03@fesb.hr>
2020-12-02 13:29:43 +0100

    Implement secure password verification using bcrypt slow hash

--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,5 +1,6 @@
-const jwt=require("jsonwebtoken");
-const config= require("../config");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
 
 
 class LoginService {
@@ -18,7 +19,9 @@ class LoginService {
     }
 
     this.logger.info("Checking password");
-    if(userRecord.password ===password){
+    const validPassword = await bcrypt.compare(password,userRecord.password);
+
+    if(validPassword){
       this.logger.info("Password correct, proceed and generate JWT");
     
       const user = {
